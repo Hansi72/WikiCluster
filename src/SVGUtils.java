756cf414c04d1303a5632d85069f3a99ed2658be
Hans Trygve <trygvehans@gmail.com>
2023-02-10 17:12:54 +0100

    mainly changed from string to stringbuilder

--- src/SVGUtils.java
+++ src/SVGUtils.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -6,19 +7,21 @@ public final class SVGUtils {
 
     //Run a python command to create a SVG using GraphViz. Requires python in working directory or PATH variables
     public static byte[] createSVG(String dotFile) {
-        System.out.println("creating SVG");
         try {
+            System.out.println("creating SVG");
             ProcessBuilder processBuilder = new ProcessBuilder("python", "GraphViz/GraphViz.py");
             processBuilder.redirectErrorStream(true); //todo remove
             Process process = processBuilder.start();
             //OutputStream outStream =
-            process.getOutputStream().write(dotFile.getBytes("UTF-8"));
+            process.getOutputStream().write(dotFile.getBytes(StandardCharsets.UTF_8));
             process.getOutputStream().close();
             //outStream.write(dotFile);
             byte[] svgBytes = process.getInputStream().readAllBytes();
             process.waitFor();
+            System.out.println("createSVG complete");
             return svgBytes;
         } catch (Exception e) {
+            System.out.println("createSVG SVG failed");
             System.out.println(e);
             return null;
         }
@@ -30,7 +33,7 @@ public final class SVGUtils {
             File dotFile = new File(filename + ".dot");
             dotFile.createNewFile();
             FileWriter fWriter = new FileWriter(filename + ".dot");
-            fWriter.write(createDotFile(adjLists, db));
+            //fWriter.write(createDotFile(adjLists, db));
             fWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,24 +41,29 @@ public final class SVGUtils {
     }
 
     //creates a dot file with the given nodes and edges.
-    public static String createDotFile(HashMap<String, LinkedList<Integer>> adjLists, DataBase db) {
+    public static String createDotFile(HashMap<String, LinkedList<String[]>> adjLists) {
         //graph settings
-        String dotString = "strict digraph {\n"
-                + "layout=twopi;\n"
-                + "overlap = scale;\n"
-                + "concentrate=true\n"
-                + "outputorder=\"edgesfirst\" \n"
-                + "overlap = false;\n"
-                + "splines = true;\n"
-                + "scale = 0.1;\n";
+        StringBuilder dotString = new StringBuilder("strict graph {\n"
+                + "layout=twopi;\n"  //twopi, circo..
+               //+ "overlap = scale;\n"
+               + "concentrate=true\n"
+               // + "outputorder=\"edgesfirst\" \n"
+               + "overlap = false;\n"
+               + "splines = true;\n"
+               // + "scale = 0.1;\n"
+                );
         //add edges
-        for (HashMap.Entry<String, LinkedList<Integer>> adjList : adjLists.entrySet()) {
-            for (int edge : adjList.getValue()){
-                dotString = dotString + "\"" + db.articlesByIndex.get(edge) + "\"" + " -> " + "\"" + adjList.getKey() + "\"" + "\n";
+        for (HashMap.Entry<String, LinkedList<String[]>> adjList : adjLists.entrySet()) {
+            for (String[] edgeNStyle : adjList.getValue()){
+                dotString.append("\"" + edgeNStyle[0] + "\"" + " -- " + "\"" + adjList.getKey() + "\" ");
+                if(edgeNStyle[1] != null){
+                    dotString.append(edgeNStyle[1]);
+                }
+                dotString.append("\n");
             }
         }
-        dotString = dotString + "}";
-        return dotString;
+        dotString.append("}");
+        return dotString.toString();
     }
 
 }
