c416682310233c5afc49e328cbf9f869f5059f24
Hans Trygve <trygvehans@gmail.com>
2023-02-10 17:11:56 +0100

    changed encoding because graphvizs' encoding was shit

--- GraphViz/GraphViz.py
+++ GraphViz/GraphViz.py
@@ -1,6 +1,7 @@
 import sys
 import graphviz
 #Takes a dot file as std input and outputs a SVG file in bytes.
-src = graphviz.Source(sys.stdin.read())
+byteInput = sys.stdin.read().encode('utf-8', 'replace')
+src = graphviz.Source(byteInput.decode())
 src.format = 'svg'
 print(src.pipe(encoding='utf-8'))
